2c3ad5a803869875d197aa8737650e7350b18edd
otto67 <otto_munthe@yahoo.no>
2021-09-27 11:47:35 +0200

    Updates

--- comboplot.py
+++ comboplot.py
@@ -9,25 +9,32 @@ def plot(solu, delta, save_to_file=False, x_min=0.0, x_max=1.0):
         X, Y = np.meshgrid(x, y)
 
         fig = plt.figure(figsize=(20, 20))
-        fig.suptitle("Simulation results \n Note that FEM solver only works for rhs=0")
+        fig.suptitle("Simulation results")
 
         ax = fig.add_subplot(2, 2, 1, projection='3d')
 
         ax.plot_surface(X, Y, solu, cmap='viridis', edgecolor='none')
         ax.set_title('Surface plot u(x,y)')
+        ax.set_ylabel('y coordinate')
+        ax.set_xlabel('x coordinate')
 
         ax = fig.add_subplot(2, 2, 2)
         ax.set_title('2D projection of u(x,y)')
+        ax.set_ylabel('j node number')
+        ax.set_xlabel('i node number')
         plt.imshow(solu, cmap='hot')
         plt.colorbar()
 
         ax = fig.add_subplot(2, 2, 3)
-        ax.set_title('Fake test vector plot of u(x,y)')
-        ax.set_ylabel('Damped oscillation')
+        ax.set_title('Fake test vector plot of (u(x,y), u(x,y)')
+        ax.set_ylabel('y coordinate')
+        ax.set_xlabel('x coordinate')
         plt.quiver(X, Y, solu, solu)
 
         ax = fig.add_subplot(2, 2, 4)
         ax.set_title('Contour lines for u(x,y)')
+        ax.set_ylabel('y coordinate')
+        ax.set_xlabel('x coordinate')
         plt.contour(X, Y, solu)
 
         if (save_to_file):
